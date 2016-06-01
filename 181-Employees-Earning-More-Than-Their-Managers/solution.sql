8a938d7e99c2dd503fed746473640b78457f1034
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-01 15:42:42 +0800

    [181. Employees Earning More Than Their Managers][Accepted]committed by LeetCode Extension

--- 181-Employees-Earning-More-Than-Their-Managers/solution.sql
+++ 181-Employees-Earning-More-Than-Their-Managers/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select a.Name as Employee from Employee a left join Employee b on a.ManagerId = b.Id where a.salary>b.salary 
+select a.Name as Employee from Employee a left join Employee b on a.ManagerId = b.Id where a.Salary>b.Salary 
