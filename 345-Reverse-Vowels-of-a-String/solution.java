4d1f68ec9c72accd64773bf26b8e4d8c05f34bca
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-01 12:03:07 +0800

    [345. Reverse Vowels of a String][Time Limit Exceeded]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -4,27 +4,23 @@ public class Solution {
         if(s == null || (len = s.length()) == 0)
             return s;
         char[]  chars = s.toCharArray();
-        int i=0, j= len-1 ;
+        int i=0, j= len-1 ,m=len/2;
         char tmp ='a',l=chars[i],r=chars[j];
-        while(i<j && i<len&& j>=0){
-            while(l!='a'&&l!='e'&&l!='i'&&l!='o'&&l!='u'){
+        while(i<j){
+            while(i<m&&l!='a'&&l!='e'&&l!='i'&&l!='o'&&l!='u'){
                 i++;
-                if(i>=len)
-                    break;
                 l = Character.toLowerCase(chars[i]);
-            }
-            while(r!='a'&&r!='e'&&r!='i'&&r!='o'&&r!='u'){
+            };
+            while(j>m&&r!='a'&&r!='e'&&r!='i'&&r!='o'&&r!='u'){
                 j--;
-                if(j<0)
-                    break;
                 r = Character.toLowerCase(chars[j]);
             }
-            if( i<j ) {
+            if( i<m&&j>m&&chars[i]!=chars[j] ) {
                 tmp = chars[i];
                 chars[i] = chars[j];
                 chars[j] = tmp;
                 i++;
-                j--
+                j--;
             }
         }
         return new String(chars);
