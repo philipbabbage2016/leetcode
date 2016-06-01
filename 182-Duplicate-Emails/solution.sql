ff87341aaeb0c4077f03ebbd668d09cfa15197d3
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-01 15:22:17 +0800

    [182. Duplicate Emails][Accepted]committed by LeetCode Extension

--- 182-Duplicate-Emails/solution.sql
+++ 182-Duplicate-Emails/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select p1.Email from Person as p1 inner join Person as p2 where p1.Email = p2.Email and P1.Id<>P2.Id 
+select distinct(p1.Email) from Person as p1 inner join Person as p2 where p1.Email = p2.Email and p1.Id<>p2.Id; 
