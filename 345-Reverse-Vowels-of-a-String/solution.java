cbf409745cd0d96730fc99de7055db1b483afbfa
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-01 11:47:06 +0800

    [345. Reverse Vowels of a String][Compile Error]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -7,21 +7,24 @@ public class Solution {
         int i=0, j= len-1 ;
         char tmp ='a',l=chars[i],r=chars[j];
         while(i<j && i<len&& j>=0){
-            
             while(l!='a'&&l!='e'&&l!='i'&&l!='o'&&l!='u'){
                 i++;
+                if(i>=len)
+                    break;
                 l = Character.toLowerCase(chars[i]);
             }
             while(r!='a'&&r!='e'&&r!='i'&&r!='o'&&r!='u'){
                 j--;
+                if(j<0)
+                    break;
                 r = Character.toLowerCase(chars[j]);
             }
-            if(i<j){
+            if( i<j ) {
                 tmp = chars[i];
                 chars[i] = chars[j];
                 chars[j] = tmp;
                 i++;
-                j--;
+                j--
             }
         }
         return new String(chars);
