b3166b549f4c3115afc26d0e8df70ed3bcee899c
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-06 17:13:02 +0800

    [176. Second Highest Salary][Accepted]committed by LeetCode Extension

--- 176-Second-Highest-Salary/solution.sql
+++ 176-Second-Highest-Salary/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select (case a.Salary when NULL then 'NULL' else a.Salary)
-from (
-select Salary from Employee order by Salary limit 1,1) a
+select (
+  select distinct Salary from Employee order by Salary Desc limit 1,1
+)as SecondHighestSalary
