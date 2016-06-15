0363f1f67260b9be862a1b25ad00b994a6ad1b34
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-16 00:41:29 +0800

    [326. Power of Three][Compile Error]committed by LeetCode Extension

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,13 +1,21 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        int i =1;
+        int i=3;
         if(n<=0)
-        return false;
+            return false;
+        if(n ==1||n==3)
+            return true;
         while(i<Integer.MAX_VALUE&&i<=n){
-            if(n == i)
+            if((n%i) !=0)
+                return false;
+            n=n/i;
+            if(n ==3|| n== 1||i=n||i=3*n){
                 return true;
-            i = i*3;
+            } 
+            i=i*3;
+            
         }
+        i=3;
         return false;
     }
 }
