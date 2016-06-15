aebed1ba20b132827d363af34d970d77ec4c1273
philipbabbage2016 <philipbabbage2016@gmail.com>
2016-06-15 14:33:44 +0800

    [191. Number of 1 Bits][Compile Error]committed by LeetCode Extension

---
+++ 191-Number-of-1-Bits/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int i =0,cnt=0 ;
+        while(i<32){
+            t = 1<<i;
+            if((n&(t)) == t)
+                cnt++;
+            i++
+        }
+        return cnt;
+    }
+}
